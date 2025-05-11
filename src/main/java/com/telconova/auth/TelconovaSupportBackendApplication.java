5d40c0fdce9cf4ba4fa517862a9fe9b39c754f68
Ricardo Contreras Garzón <102176435+RickContreras@users.noreply.github.com>
2025-05-11 18:11:48 +0000

    🎉 feat: Configuración inicial del proyecto
    ✅ Configuración de Docker a nivel de entorno de desarrollo:
       - Integración con PostgreSQL y Redis.
    
    ⚙️ Configuración de Maven:
       - Inclusión de dependencias esenciales (Spring Boot, GraphQL, JPA, etc.).
    
    🔒 Configuración de seguridad:
       - Implementación básica con Spring Security.
    
    📂 Configuración de base de datos:
       - Conexión a PostgreSQL con Hibernate y JPA.
    
    🚀 Configuración de GraphQL:
       - Activación del servlet y mapeo inicial en `/graphql`.
    
    🛠️ Otros ajustes:
       - Configuración de logging para depuración de Spring Security.
       - Configuración del servidor en el puerto `8080`.

---
+++ src/main/java/com/telconova/auth/TelconovaSupportBackendApplication.java
@@ -0,0 +1,13 @@
+package com.telconova.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelconovaSupportBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelconovaSupportBackendApplication.class, args);
+	}
+
+}
